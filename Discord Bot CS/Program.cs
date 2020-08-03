bc08e59fe52425141d0e4ab4ebdfe26a04d0cf4b
malmal200 <malachybyrne1@gmail.com>
2020-08-03 03:05:44 +0100

    Add project files.

---
+++ Discord Bot CS/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json.Linq;
+
+namespace Discord_Bot_CS
+{
+    internal class Program
+    {
+        private static HttpClient client = new HttpClient();
+        private DiscordSocketClient _client;
+        private static void Main() => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.MessageReceived += MessageReceived;
+            var token = getKey("discord");
+            Console.WriteLine(token);
+            Console.ReadLine();
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task<Task> MessageReceived(SocketMessage msg)
+        {
+            if(!msg.ToString().StartsWith("!"))
+            {
+                return Task.CompletedTask;
+            }
+            var content = msg.ToString().Substring(1);
+            var command = content.Split(" ");
+            switch(command[0])
+            {
+                case "hello":
+                    await msg.Channel.SendMessageAsync("Hello " + msg.Author.Username);
+                    break;
+                case "kanye":
+                    await msg.Channel.SendMessageAsync(Randoms.Kanye(client));
+                    break;
+                case "dog":
+                case "cat":
+                case "giraffe":
+                case "monkey":
+                    await msg.Channel.SendMessageAsync(await Randoms.ImageAsync(client, command[0], getKey("imgur")));
+                    break;
+                default:
+                    await msg.Channel.SendMessageAsync("Command not found");
+                    break;
+            }
+            return Task.CompletedTask;
+        }
+
+        private string getKey(string platform)
+        {
+            using (StreamReader r = new StreamReader("keys.json"))
+            {
+                var json = JObject.Parse(r.ReadToEnd());
+                return (string) json[platform];
+            }
+        }
+    }
+}
---
+++ Discord Bot CS/Randoms.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.Http;
+using System.Collections;
+using Newtonsoft.Json.Linq;
+using System;
+using Discord;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Discord_Bot_CS
+{
+	public class Randoms
+	{
+
+		private static Dictionary<string, JObject> imageArchives = new Dictionary<string, JObject>();
+		private static Random random = new Random();
+		public static string Kanye(HttpClient client)
+		{
+			var req = JObject.Parse(client.GetStringAsync("https://api.kanye.rest").Result);
+			return (string) req["quote"];
+		}
+
+		public static async System.Threading.Tasks.Task<string> ImageAsync(HttpClient client, string tag, string key)
+        {
+			var req = new JObject();
+			if(imageArchives.ContainsKey(tag) && DateTime.Now.Subtract((DateTime)imageArchives[tag]["datetime"]).TotalMinutes < 60)
+            {
+				req = imageArchives[tag];
+            }
+			else
+            {
+				Dictionary<string, string> auth = new Dictionary<string, string>();
+				auth.Add("Authorization", "Client-ID " + key);
+				var headers = JsonConvert.SerializeObject(auth);
+				var url = "https://api.imgur.com/3/gallery/t/" + tag;
+				HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, url);
+				message.Headers.Add("Authorization", "Client-ID " + key);
+				var response = await client.SendAsync(message).Result.Content.ReadAsStringAsync();
+				Console.WriteLine("begin parsing");
+				req = JObject.Parse(response);
+				Console.WriteLine("finished parsing");
+				req.Add("datetime", DateTime.Now);
+				imageArchives.Add(tag, req);
+            }
+			//while (true)
+			{
+				try
+				{
+					Console.WriteLine("attempting");
+					Console.WriteLine(req["data"]["items"][random.Next(0, 39)]);
+					return (string)req["data"]["items"][random.Next(0, 39)]["images"][0]["link"];
+				}
+				catch (NullReferenceException)
+				{
+					Console.WriteLine("failure");
+					return "Failed";
+				}
+			}
+        }
+	}
+}
